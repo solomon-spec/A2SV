2855aa24bb914eec637077720cc3e95c8a7dc1f3
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-01-03 15:54:42 +0300

    Time: 212 ms (38.76%), Space: 13.3 MB (100.00%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def buildArray(self, nums):
-        nums2 = [0 for i in nums]
-        for i in range(len(nums)):
-            nums2[i] = nums[nums[i]]
+        nums2 = []
+        for i in nums:
+            nums2.append(nums[i])
             
         return nums2
         """
