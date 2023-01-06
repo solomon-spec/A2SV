fd6e300f83a7f4b18451b9e8548f53a921c7c98d
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-01-06 09:09:33 +0300

    Time: 448 ms (5.08%), Space: 15 MB (33.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        result = nums[0]
+        result = 0
         for number in nums:
-            if number == result:
-                count+=1
-            else:
-                if count==0:
-                    result=number
-                else:
-                    count-=1
+            if count == 0:
+                result = number
+            count+= (1 if number == result else -1)
         return result
+    
