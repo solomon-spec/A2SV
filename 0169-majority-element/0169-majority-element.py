95570275eafcb9ee5efcf8c3857651a137cd4884
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-01-06 08:47:36 +0300

    Time: 217 ms (50.95%), Space: 15.2 MB (16.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,6 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        
-        return nums[int(len(nums)/2)]
+        count  = 0
+        result  = nums[0]
+        for number in nums:
+            if number == result:
+                count+=1
+            else:
+                if count ==0:
+                    result=number
+                else:
+                    count-=1
+        return result
