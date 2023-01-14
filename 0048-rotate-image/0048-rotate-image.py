f1bdd02afc0573826d206081106cb459015c8192
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-01-14 06:50:13 +0300

    Time: 14 ms (98.33%), Space: 13.3 MB (93.39%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        dic = defaultdict(list)
-        row_len = len(matrix)-1
         for row in range(len(matrix)):
             for column in range(len(matrix[0])):
-                dic[row_len - row].append(matrix[row][column])    
+                if row >= column:
+                    matrix[row][column],matrix[column][row] = matrix[column][row], matrix[row][column]
         
-        for column in dic:
-            for num in range(row_len+1):
-                matrix[num][column] = dic[column][num]
+        for row in range(len(matrix)):
+            matrix[row].reverse()
