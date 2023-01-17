936375d0ff539763f38ac4f492bd231788f6b5e2
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-01-17 18:09:29 +0300

    Time: 94 ms (75.39%), Space: 14 MB (55.96%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,9 +5,22 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
-        for i in range(len(names)):
+        """for i in range(len(names)):
             for j in range(len(names)-1):
                 if heights[j] < heights[j+1]:
                     heights[j],  heights[j+1] = heights[j + 1], heights[j]
                     names[j], names[j+1] = names[j+1], names[j]
-        return names
+        return names"""
+        dic ={}
+       
+        answer = []
+        for i in range(len(names)):
+            dic[heights[i]] = names[i]
+            
+        heights.sort()
+        
+        for i in range(len(names)-1,-1,-1):
+            answer.append(dic[heights[i]])
+        
+        return answer
+        
