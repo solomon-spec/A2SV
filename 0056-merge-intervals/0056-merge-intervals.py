f5a1096341add70bf0c7f661e4921de6bc87926a
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-10 09:01:37 +0300

    Time: 121 ms (53.51%), Space: 19.7 MB (8.60%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        maxx = 0
+        answer = []
+        same = set()
+        diffrent = set()
+        for i in intervals:
+            if i[1] > maxx:
+                maxx = i[1]
+        arr = [0]*maxx
+        arr += [0]
+        for i in intervals:
+            arr[i[0]] += 1
+            arr[i[1]] -= 1
+            if arr[i[0]] == arr[i[1]]:
+                same.add(i[0])
+        for i in range(len(arr)):
+            arr[i] = arr[i] if i == 0 else arr[i-1] + arr[i]
+    
+        for num in same:
+            if arr[num] != 0:
+                diffrent.add(num)
+        arr2 = sorted(list(same))
+        index = 0
+        while index < len(arr):
+            if arr[index] > 0:
+                initial = index
+                while index < len(arr) and arr[index] > 0:
+                    index += 1
+                answer.append([initial,index])
+                index += 1
+            else:
+                if index in same and index not in diffrent:
+                    answer.append([index,index])
+                index += 1
+        return answer
+                
+        
+        
