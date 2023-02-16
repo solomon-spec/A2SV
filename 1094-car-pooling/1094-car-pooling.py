8958c245b217c4b0e7948aaf5c1721ba73826f3e
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-16 16:00:29 +0300

    Time: 88 ms (29.52%), Space: 14.4 MB (35.24%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,9 +1,11 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        arr = [0]*(max(trips, key = lambda x: x[2])[2] + 1)
+        minn = (min(trips, key = lambda x: x[1])[1])
+        arr = [0]*((max(trips, key = lambda x: x[2])[2] + 1) - minn)
+        print(arr)
         for i in trips:
-            arr[i[1]] += i[0]
-            arr[i[2]] -= i[0]
+            arr[i[1] - minn] += i[0]
+            arr[i[2] - minn] -= i[0]
         summ = 0
         for i in range(len(arr)):
             summ += arr[i]
