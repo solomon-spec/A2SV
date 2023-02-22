42c9df98d70deaddbcf0556345bb92232e9ff872
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-22 11:50:03 +0300

    Time: 1662 ms (18.49%), Space: 27.4 MB (68.39%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.py
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.py
@@ -3,16 +3,20 @@ class Solution:
         l = r = summ = 0
         maxx = 0
         dic = defaultdict(int)
+        
         while r < len(nums):
+            
             if nums[r] not in dic:
                 dic[nums[r]]  += 1
                 summ += nums[r]
                 maxx = max(maxx, summ)
                 r += 1
+                
             else:
                 dic[nums[l]] -= 1
                 if dic[nums[l]] == 0:
                     del dic[nums[l]]
                     summ -= nums[l]
                     l+= 1
+                    
         return maxx
