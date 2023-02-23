b5473d59efc2018ba4cce3b16f48485d2e150e48
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-23 18:49:59 +0300

    Time: 810 ms (89.16%), Space: 20.6 MB (72.44%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -5,7 +5,6 @@ class Solution:
             if num % 2 == 1:
                 arr.append(i)
         arr.append(len(nums))
-        print(arr)
         answer = 0
         for i in range(1,len(arr)- k):
             answer += (arr[i]-arr[i-1])*(arr[i + k]-arr[i+k-1])
