d61ba8af268918328814aed26b38f9e374ec808c
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-24 10:42:11 +0300

    Time: 62 ms (71.71%), Space: 19.1 MB (5.05%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,38 +1,33 @@
-class MinStack(object):
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+class MinStack:
 
     def __init__(self):
-        self.minn = []
-        self.arr = []
+        self.head = None 
+        self.head2  = None
 
-    def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        self.arr.append(val)
-        if not self.minn or val <= self.minn[-1]:
-            self.minn.append(val)
-            
-    def pop(self):
-        """
-        :rtype: None
-        """
-        x = self.arr.pop()
-        if x == self.minn[-1]:
-            self.minn.pop()
-        
-    def top(self):
-        """
-        :rtype: int
-        """
-        return self.arr[-1]
+    def push(self, val: int) -> None:
+        cur = Node(val)
+        cur.next = self.head
+        self.head = cur
+        if not self.head2 or val <=self.head2.val:
+            minn = Node(val)
+            minn.next = self.head2
+            self.head2 = minn
 
-    def getMin(self):
-        """
-        :rtype: int
-        """
-        return self.minn[-1]
-        
+    def pop(self) -> None:
+        if self.head.val == self.head2.val:
+            self.head = self.head.next
+            self.head2 = self.head2.next
+        else:
+            self.head = self.head.next
+    def top(self) -> int:
+        return self.head.val
+
+    def getMin(self) -> int:
+        return self.head2.val
 
 
 # Your MinStack object will be instantiated and called as such:
