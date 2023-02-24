f99f790752352f39ef0b69dc3e75f99a314db3c5
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-24 09:25:40 +0300

    Time: 270 ms (98.43%), Space: 15.9 MB (64.96%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
+        arr = [-1]
+        for i,num in enumerate(nums):
+            if num == 1:
+                arr.append(i)
+        arr.append(len(nums))
+        answer = 0
+        print(arr)
+        if k == 0:
+            for i in range(1,len(arr)):
+                num = (arr[i] - arr[i - 1])
+                answer += (num*(num - 1))/2
+            return int(answer)
+        for i in range(1,len(arr)- k):
+            answer += (arr[i]-arr[i-1])*(arr[i + k]-arr[i+k-1])
+        return answer
