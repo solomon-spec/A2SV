f83a295a626159f4067955b5cc8e92b93b2cf4dd
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-24 10:54:06 +0300

    Time: 316 ms (92.50%), Space: 16.5 MB (60.00%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeStars(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = []
+        for i in s:
+            if i != "*":
+                stack.append(i)
+            else:
+                stack.pop()
+        return "".join(stack)
