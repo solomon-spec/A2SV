e26d49436ca3409b0c44ef128b70edfedad6c7b9
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-02-28 15:19:55 +0300

    Time: 8093 ms (5.41%), Space: 43.4 MB (5.42%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -4,5 +4,7 @@ class Solution:
             if x == 0:
                 return(["0"])
             else:
-                return other(x - 1) +["1"] + list(reversed(["0" if i == "1" else "1" for i in other(x - 1)]))
-        return other(n-1)[k - 1]
+                val = other(x - 1)
+                return other(x - 1) +["1"] + list(reversed(["0" if i == "1" else "1" for i in val]))
+        vall = other(n-1)
+        return vall[k - 1]
