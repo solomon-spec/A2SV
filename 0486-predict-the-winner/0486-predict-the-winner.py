b966e845a06baa67027c94cfecb87444ed034400
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-03-01 16:59:09 +0300

    Time: 35 ms (83.32%), Space: 14.4 MB (6.84%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,9 +1,47 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         dic = {}
-        @cache
-        def check(start,end):
-            if start == end:
-                return nums[start]
-            return max(nums[start] - check(start+1,end),nums[end] - check(start,end-1))
-        return 0<=check(0,len(nums)-1)
+        def check(start,end,player1):
+            if (start,end,player1) not in dic:
+                
+                if start == end:
+                    if player1: 
+                        dic[(start,end,player1)] = [nums[start],0]
+                        return [nums[start],0]
+                    
+                    dic[(start,end,player1)] = [0,nums[start]]
+                    return [0,nums[start]]
+
+                if player1:
+                    answer1 = check(start + 1,end,not player1).copy()
+                    answer1[0] += nums[start]
+
+                    answer2 = check(start,end - 1,not player1).copy()
+                    answer2[0] += nums[end]
+
+                    if answer1[0] > answer2[0]:
+                        dic[(start,end,player1)] = answer1
+                        return answer1
+                    
+                    dic[(start,end,player1)] = answer2
+                    return answer2
+
+                answer1 = check(start + 1,end,not player1).copy()
+                answer1[1] += nums[start]
+
+                answer2 = check(start,end - 1,not player1).copy()
+                answer2[1] += nums[end]
+
+                if answer1[1] > answer2[1]: 
+                    dic[(start,end,player1)] = answer1
+                    return answer1
+                
+                dic[(start,end,player1)] = answer2
+                return answer2
+            
+            else:
+                return dic[(start,end,player1)]
+        
+        answer = check(0,len(nums)-1,True)
+        if answer[0] >= answer[1]: return True
+        return False
