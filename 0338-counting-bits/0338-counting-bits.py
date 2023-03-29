2ec6c24c6f4d0129bcaacf1f3c52b1babffa5672
solomon-spec <70583058+solomon-spec@users.noreply.github.com>
2023-03-29 08:14:02 -0700

    Time: 89 ms (57.84%), Space: 20.9 MB (19.07%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,12 +1,12 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        answ = []
-        for i in range(n + 1):
-            ans = 0 
-            cur = i
-            while cur > 0:
-                if cur % 2 == 1: ans += 1
-                cur//= 2
-            answ.append(ans)
-        return answ
-            
+        ans = [0,1]
+        if n == 0:
+            return [0]
+        for i in range(2,n + 1):
+            if i % 2 == 0:
+                ans.append(ans[i//2])
+            else:
+                ans.append(ans[i//2] + 1)
+        return ans
+           
