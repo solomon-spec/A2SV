c0f17150ad2e811dc2a05438227555daf02c583e
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-05-09 19:18:36 +0300

    Time: 117 ms (18.81%), Space: 17.6 MB (23.44%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dic = defaultdict(list)
+        pre_req = [0]*numCourses
+        for i,j in prerequisites:
+            dic[j].append(i)
+            pre_req[i] += 1
+        ans = []
+        queue = deque()
+        for i in range(numCourses):
+            if pre_req[i] == 0: queue.append(i)
+        visited = set([i for i in queue])
+        while queue:
+            x = queue.popleft()
+            ans.append(x)
+            for course in dic[x]:
+                pre_req[course] -= 1
+                if pre_req[course] == 0: queue.append(course)
+        if len(ans) == numCourses:
+            return True
+        return False
