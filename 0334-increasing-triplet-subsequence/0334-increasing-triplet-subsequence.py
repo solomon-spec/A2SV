063fa823c31a0a5befe781bb51719f79d9558bd9
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-05-25 16:11:17 +0300

    Time: 1062 ms (62.25%), Space: 32.5 MB (16.65%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -1,12 +1,13 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        pre_min = []
-        post_max = deque()
-        for i in range(len(nums)):
-            if pre_min: pre_min.append(min(pre_min[-1],nums[i]))
-            else: pre_min.append(nums[i])
-            if post_max: post_max.appendleft(max(post_max[0],nums[-i-1]))
-            else: post_max.append(nums[-1])
-        for i in range(len(nums)):
-            if pre_min[i] < nums[i] < post_max[i]: return True
+        
+        if len(nums) < 3: return False
+        abs_min = nums[0]
+        cur_min = inf
+        for i in nums:
+            if i <= abs_min: abs_min = i
+            elif i < cur_min: cur_min = i
+            elif abs_min < cur_min < i: return True
         return False
+
+        
