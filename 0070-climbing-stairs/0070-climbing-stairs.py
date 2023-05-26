5d13b6557eeacf12eaa77ac0bc9009c0ce53998f
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-05-26 18:25:53 +0300

    Time: 56 ms (5.97%), Space: 16.2 MB (28.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,6 @@
 class Solution:
-    @cache
     def climbStairs(self, n: int) -> int:
+        arr = [1,2]
         if n < 3: return n
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        for i in range(2,n): arr.append(arr[-1]+arr[-2])
+        return arr[-1]
