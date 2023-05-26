5b05bb7a791b514a658750a5770046fa2c90944f
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-05-26 18:27:25 +0300

    Time: 52 ms (8.32%), Space: 16.4 MB (8.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,5 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         arr = [1,2]
         if n < 3: return n
-        for i in range(2,n): arr.append(arr[-1]+arr[-2])
+        for i in range(2,n):
+            
+            arr[-2],arr[-1] = arr[-1],arr[-1]+arr[-2]
         return arr[-1]
