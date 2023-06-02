f1cd775b024f824693b0bec8991564c9d584e06f
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-06-02 22:11:49 +0300

    Time: 120 ms (47.22%), Space: 17.1 MB (7.41%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.py
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -1,11 +1,9 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        memo = defaultdict(int)
-        memo[(0,0)] = poured -1
+        @lru_cache
         def dp(x,y):
+            if x == 0 and y == 0: return poured -1
             if y > x or y < 0: return 0
-            if (x,y) not in memo:
-                memo[(x,y)] = max(0,dp(x-1,y)/2) + max(0,dp(x-1,y-1)/2) -1
-            return memo[(x,y)]
+            return max(0,dp(x-1,y)/2) + max(0,dp(x-1,y-1)/2) -1
         return min(1,dp(query_row,query_glass) + 1)
         
