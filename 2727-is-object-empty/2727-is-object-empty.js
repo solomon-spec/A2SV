4262d16cc7d7918148faa97067ace7bf7608c661
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-06-14 05:00:39 -0700

    Time: 56 ms (71.93%), Space: 42.2 MB (89.10%) - LeetHub

--- 2727-is-object-empty/2727-is-object-empty.js
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -3,9 +3,6 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    let count = 0
-    for(let i of Object.keys(obj)){
-        count += 1;
-    }
-    return count == 0
+
+    return Object.keys(obj).length == 0
 };
