02f81d9edb62a24267dd1fcbd23f82ba4acbd559
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-08-02 15:01:01 +0300

    Time: 49 ms (83.55%), Space: 16.4 MB (72.51%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,22 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res  = []
-        
-        def backtrack(num,arr):
+        numm = 0
+        def backtrack(num):
+            nonlocal numm
             if len(num) == len(nums):
                 res.append(num.copy())
                 return
             
-            for i in range(len(arr)):
-                num.append(arr[i])
-                backtrack(num,arr[:i] + arr[i+1:])
-                num.pop()
+            for i in range(len(nums)):
+                if numm & (1<<i) == 0:
+                    num.append(nums[i])
+                    numm |= (1<<i)
+                    backtrack(num)
+                    num.pop()
+                    numm &= ~(1<<i)
             
         
         
-        backtrack([],nums)
+        backtrack([])
         return res
-            
