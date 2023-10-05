673dfccc787f68171434e81587bed627a903935b
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-10-05 07:39:46 +0300

    Time: 17 ms (88.64%), Space: 12.7 MB (80.31%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -6,29 +6,29 @@ public:
     
     int shortestPath(vector<vector<int>>& grid, int k) {
         memset(lowest, -1, sizeof lowest);
-        queue<vector<int>> bfs;
+        queue<array<int, 4>> bfs;
         int row = grid.size();
         int col = grid[0].size();
         bfs.push({0,0,0,0});
    
         while(bfs.size()){
 
-            vector<int> cur = bfs.front();
+            auto [cur0,cur1,cur2,cur3] = bfs.front();
             bfs.pop();
             
-            if(cur[2] == row - 1 && cur[3] == col -1 && k >= cur[1]) return cur[0];
-            if(lowest[cur[2]][cur[3]] != -1 && lowest[cur[2]][cur[3]] <= cur[1]) continue;
-            lowest[cur[2]][cur[3]] = cur[1];
+            if(cur2 == row - 1 && cur3 == col -1 && k >= cur1) return cur0;
+            if(lowest[cur2][cur3] != -1 && lowest[cur2][cur3] <= cur1) continue;
+            lowest[cur2][cur3] = cur1;
             
             for(auto dir: dire){
-                int r = dir[0] + cur[2];
-                int c = dir[1] + cur[3];
+                int r = dir[0] + cur2;
+                int c = dir[1] + cur3;
                 
                 if(0 <= r && r < row && 0 <= c && c < col){
-                    int x = cur[1] + grid[r][c];
+                    int x = cur1 + grid[r][c];
                    
                     if((lowest[r][c] == -1) || (x < lowest[r][c] && x <= k )){
-                        bfs.push({cur[0]+1,x,r,c});
+                        bfs.push({cur0+1,x,r,c});
                         
                     }
                 }
