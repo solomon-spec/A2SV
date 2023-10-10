2b4a497162638a6504ea54bda413603f223f389d
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-10-10 18:33:41 +0300

    Time: 0 ms (100.00%), Space: 6.5 MB (33.63%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,54 +1,41 @@
-#define ll long long
-ll power(ll x, unsigned int y, int p) 
-{ 
-    int res = 1;   
- 
-    x = x % p; 
-  
-    if (x == 0) return 0; 
- 
-    while (y > 0) 
-    { 
-        if (y & 1) 
-            res = (res*x) % p; 
-        y = y>>1; 
-        x = (x*x) % p; 
-    } 
-    return res; 
-} 
 class Solution {
 public:
     
     int strStr(string haystack, string needle) {
-        long long hash = 0;
-        long long ned = 0;
-        long long mod = pow(10,9)+7;
-    
-        if(haystack.size() < needle.size()) return -1;
-        int siz = needle.size()-1;
-        int cur = siz;
-        for(auto a: needle){
-            ned += ((a - 'a'+1)*power(26,cur,mod)) % mod;
-            cur--;
-        }
-        ned = ned % mod;
-        cur = siz;
-        
-        for(int i = 0; i < haystack.size(); i++){
-            if(i <= siz ){
-                
-                hash = (hash + (haystack[i] - 'a'+1)*power(26,cur,mod)) % mod;
-                cur--;
+        vector<int> ans;
+        bool b = false;
+        int pre = 0;
+        int cur = 1;
+        ans.push_back(0);
+        while(cur < needle.size()){
+            if(needle[cur]==needle[pre]){
+                ans.push_back(pre+1);
+                cur++;pre++;
             }
             else{
+                if(pre == 0){
+                    ans.push_back(0);
+                    cur++;
+                }
+                else{
+                    pre = ans[pre-1];
+                }
+            }
+        }
+        for(auto a: ans)cout<<a<<" ";
+        cout<<endl;
+        int j = 0;
+   
+        int i = 0;
+        while(i < haystack.size()){
+            if(haystack[i] == needle[j]){j++;i++;}
+            else{
+                if (j != 0){ j = ans[j-1];}
+                else i++;
                 
-                hash = (hash * 26);
-                hash -= (((haystack[i-siz-1] - 'a' + 1)*power(26,siz+1,mod)) % mod) % mod;
-                hash = (hash +(haystack[i] - 'a'+1)) % mod;
-                if (hash < 0)hash = mod + hash;
             }
             
-            if (hash == ned) return i-siz;
+            if (j == needle.size()) return i-j;
             
         }
         return -1;
