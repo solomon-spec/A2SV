6b16a75ca6ad40628ed910ebd3804d7804fa3d41
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-10-17 15:32:39 +0300

    Time: 39 ms (52.08%), Space: 9.6 MB (63.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,17 +2,19 @@ class Solution {
     int dp[2005][20];
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        memset(dp,0,sizeof dp);
-        dp[-nums[0]+1000][0] = 1;
-        dp[nums[0]+1000][0] += 1;
+        memset(dp,0,4*4*nums.size());
         int sum = 0;
         for(auto a: nums)sum += a;
+        if(target > sum || target < -sum)return 0;
+        dp[-nums[0]+sum][0] = 1;
+        dp[nums[0]+sum][0] += 1;
         for(int i = 1; i < nums.size();i++){
             for(int s = -sum ; s <= sum; s++){
-               if(s-nums[i] >= -1000) dp[s+1000][i] += dp[s-nums[i]+1000][i-1];
-               if(s+nums[i] <= 1000) dp[s+1000][i] += dp[s+nums[i]+1000][i -1];
+               if(s-nums[i] >= -sum) dp[s+sum][i] += dp[s-nums[i]+sum][i-1];
+               if(s+nums[i] <= sum) dp[s+sum][i] += dp[s+nums[i]+sum][i -1];
             }
         }
-        return dp[1000+target][nums.size()-1];
+    
+        return dp[sum+target][nums.size()-1];
     }
 };
