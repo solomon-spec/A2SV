b378b6e6a686d528082cabceb2d09f929762733c
Solomon <70583058+solomon-spec@users.noreply.github.com>
2023-10-18 11:49:39 +0300

    Time: 7 ms (84.96%), Space: 7.3 MB (87.07%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,12 @@
 class Solution {
-    int dp[5001][301];
+    int dp[5001];
 public:
     int change(int amount, vector<int>& coins) {
-        memset(dp,0,sizeof dp);
-        sort(coins.begin(),coins.end());
-        for(int i = 0 ; i < coins.size(); i++) dp[0][i] = 1;
-   
-        for(int i = 0; i < coins.size();i++){
-           
-            for(int j = 1; j <= amount;j++){
-                int tar = j - coins[i];
-                if( tar < 0 && i > 0) {
-                    dp[j][i] += dp[j][i-1];
-                }
-                else if(i > 0)dp[j][i] += (dp[j][i-1] + dp[tar][i]);
-                else if(tar >= 0)dp[j][i] += dp[tar][i];
-              
-            }
-
-        }   
-
-        return dp[amount][coins.size()-1];
+        memset(dp,0,sizeof (amount+1)*4);
+        dp[0] = 1;
+        for(int i = 0; i < coins.size();i++)
+            for(int j = coins[i]; j <= amount;j++)dp[j] += dp[j - coins[i]];
+        return dp[amount];
         
     }
 };
